e5196adf678343a20687b223e8c21844a3aced96
05kiran27 <kiran.goje1@gmail.com>
2024-08-27 15:01:18 +0530

    dist

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5000',
     credentials: true,
 }));
 
