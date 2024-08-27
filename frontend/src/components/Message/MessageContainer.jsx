5c848d472bb11db46dba261d921b37721eb27fa2
05kiran27 <kiran.goje1@gmail.com>
2024-08-27 16:41:18 +0530

    chat final

--- frontend/src/components/Message/MessageContainer.jsx
+++ frontend/src/components/Message/MessageContainer.jsx
@@ -14,6 +14,7 @@ const MessageContainer = () => {
     }, [setSelectedConversation]);
 
     return (
+        
         <div className='md:min-w-[450px] flex flex-col'>
             {selectedConversation ? (
                 <>
